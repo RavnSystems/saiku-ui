beb25101e65057bcdb27813d9e9f6aea549a6013
Adam <adam@ravn.co.uk>
2014-05-09 16:30:20 +0100

    branding

--- js/saiku/Settings.js
+++ js/saiku/Settings.js
@@ -18,7 +18,7 @@
  * Change settings here
  */
 var Settings = {
-    VERSION: "Saiku 2.6-SNAPSHOT",
+    VERSION: "Ravn Analytics Explorer 2.6-SNAPSHOT",
     BIPLUGIN: false,
     BIPLUGIN5: false,
     BASE_URL: "",
